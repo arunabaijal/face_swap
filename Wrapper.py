702a62418a0c2b8b352f65ff830a19a8e0053d12
Aruna Baijal <aruna11baijal@gmail.com>
2020-03-31 00:07:41 -0400

    arg change

--- Wrapper.py
+++ Wrapper.py
@@ -5,20 +5,20 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Face Swap')
 
-    parser.add_argument('-m', '--mode', default='trian', type=str,
+    parser.add_argument('--mode', default='trian', type=str,
                         help='path to input image')
-    parser.add_argument('-i', '--ref_path', default='Data/Rambo.jpg', type=str,
+    parser.add_argument('--img_path', default='Data/Rambo.jpg', type=str,
                         help='path to reference image(texture ref)')
-    parser.add_argument('-v', '--video_path', default='Data/data1.mp4', type=str,
+    parser.add_argument('--video_path', default='Data/data1.mp4', type=str,
                         help='path to video')
 
-    if parser.parse_args().m == 'trian':
-        if parser.parse_args().i == 'swap':
-            swap_face_part1.main(True, parser.parse_args().i, parser.parse_args().v)
+    if parser.parse_args().mode == 'trian':
+        if parser.parse_args().img_path == 'swap':
+            swap_face_part1.main(True, parser.parse_args().img_path, parser.parse_args().video_path)
         else:
-            swap_face_part1.main(False, parser.parse_args().i, parser.parse_args().v)
-    elif parser.parse_args().m == 'tps':
-        if parser.parse_args().i == 'swap':
-            face_swap_tpl.start(True, parser.parse_args().i, parser.parse_args().v)
+            swap_face_part1.main(False, parser.parse_args().img_path, parser.parse_args().video_path)
+    elif parser.parse_args().mode == 'tps':
+        if parser.parse_args().img_path == 'swap':
+            face_swap_tpl.start(True, parser.parse_args().img_path, parser.parse_args().video_path)
         else:
-            face_swap_tpl.start(False, parser.parse_args().i, parser.parse_args().v)
+            face_swap_tpl.start(False, parser.parse_args().img_path, parser.parse_args().video_path)
